195dfa167c53ff2e723f194aa915b52ee1c3fea5
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-08-31 06:59:43 +0900

    Update 03_interval_220831.sql

--- 02_basic_date_n_timestamp_n_time_n_interval/03_interval_220831.sql
+++ 02_basic_date_n_timestamp_n_time_n_interval/03_interval_220831.sql
@@ -11,14 +11,14 @@ select pg_typeof(to_date('2022-01-01', 'yyyy-mm-dd') + 2) as date_01 -- type : d
 	, to_date('2022-01-01', 'yyyy-mm-dd') - interval '2 days' as date_to_timestamp_03 -- ret : 2021-12-30 00:00:00.000
 	, pg_typeof(to_date('2022-01-01', 'yyyy-mm-dd') - interval '2 days') as date_to_timestamp_04; -- type : timestamp
 	
--- Q1-3. to_date - to_date = integer 연산 확인
+-- Q1-3. to_date - to_date 연산 시, integer type 변환 확인
 select to_date('2022-01-01', 'yyyy-mm-dd') - to_date('2022-01-03', 'yyyy-mm-dd') as interval_01 -- ret : -2
 	, pg_typeof(to_date('2022-01-01', 'yyyy-mm-dd') - to_date('2022-01-03', 'yyyy-mm-dd')) as interval_02; -- type : integer
 select to_date('2022-01-03', 'yyyy-mm-dd') - to_date('2022-01-01', 'yyyy-mm-dd') as interval_03 -- ret : 2
 	, pg_typeof(to_date('2022-01-03', 'yyyy-mm-dd') - to_date('2022-01-01', 'yyyy-mm-dd')) as interval_04;  -- type : integer
 
 
--- Q2-1. to_timestamp 연산 불가능 확인
+-- Q2-1. to_timestamp 연산 불가 확인
 select to_timestamp('2022-01-01', 'yyyy-mm-dd hh24:mi:ss') + 2 as timestamp_01; -- ret : Error
 	
 -- Q2-2. to_timestamp에 interval 사용 시, 연산 가능 확인
@@ -27,7 +27,7 @@ select to_timestamp('2022-01-01', 'yyyy-mm-dd hh24:mi:ss') + interval '2' as tim
 	, to_timestamp('2022-01-01', 'yyyy-mm-dd hh24:mi:ss') + interval '2 days' as timestamp_04 -- ret : 2022-01-03 00:00:00.000 +0900
 	, to_timestamp('2022-01-01', 'yyyy-mm-dd hh24:mi:ss') + interval '2' day as timestamp_05; -- ret : 2022-01-03 00:00:00.000 +0900
 	
--- Q2-3. to_timestamp - to_timestamp = interval 연산 확인
+-- Q2-3. to_timestamp - to_timestamp 연산 시, interval type 변환 확인
 select to_timestamp('2022-01-01', 'yyyy-mm-dd hh24:mi:ss') 
 	- to_timestamp('2022-01-03', 'yyyy-mm-dd hh24:mi:ss') as interval_05  -- ret : -2 days
 	, pg_typeof(to_timestamp('2022-01-01', 'yyyy-mm-dd hh24:mi:ss') 
